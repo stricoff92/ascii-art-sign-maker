70c9703012275b0ec7107882419868e150640884
Jon <stricoff92@gmail.com>
2025-10-16 09:43:38 -0400

    wip

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # ascii-art-sign-maker
-Convert images into ASCII art for printing on signs 
+Convert images into ASCII art for printing on signs
+
+## Installation
+
+```bash
+# create virtual environment
+python3 -m venv env
+source env/bin/activate
+
+# install deps
+pip install -r requirements.txt
+
+```
+
+## usage
+
+```bash
+
+# help message
+python3 app.py -h
+
+```
+
--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 
+import argparse
 import datetime as dt
-import sys
-
 
 # Loggers
 def log(msg: str, level):
     print(f"[{level}] [{dt.datetime.now().isoformat()}] - {msg}")
-
 def log_debug(msg: str):
     log(msg, "DEBUG")
-
 def log_info(msg: str):
     log(msg, "INFO")
-
 def log_warn(msg: str):
     log(msg, "WARN")
-
 def log_err(msg: str):
     log(msg, "ERR")
 
@@ -23,8 +18,24 @@ def log_err(msg: str):
 
 
 # App Entry Point
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="ASCII Art Sign Maker")
+    parser.add_argument("path_to_file", type=str, help="Full path to input image")
+    parser.add_argument("pixels_per_char", type=int, help="Number of pixels (width and height) per character")
+    parser.add_argument("chars_per_page_width", type=int, help="Number of character columns per page")
+    parser.add_argument("chars_per_page_height", type=int, help="Number of character rows per page")
+    return parser.parse_args()
+
+
 def app():
     log_info("script starting")
+
+    args = parse_args()
+    log_debug(f"path_to_file: {args.path_to_file}")
+    log_debug(f"pixels_per_char: {args.pixels_per_char}")
+    log_debug(f"chars_per_page_width: {args.chars_per_page_width}")
+    log_debug(f"chars_per_page_height: {args.chars_per_page_height}")
 
 if __name__ == "__main__":
     app()
